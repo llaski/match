1651ffb30a176cc4d070f12b35731bd8f89cc23c
Larry Laski <larry.laski@gmail.com>
2014-04-15 09:04:51 -0400

    backbone tutorials

--- app/views/backbone/index.blade.php
+++ app/views/backbone/index.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <title>Backbone</title>
 
+        <script id="personTemplate" type="text/template">
+            <strong><%= name %></strong> (<%= age %>) - <%= occupation %>
+        </script>
         <script src="/js/underscore.js"></script>
         <script src="/js/jquery.js"></script>
         <script src="/js/backbone.js"></script>
--- public/js/main.js
+++ public/js/main.js
@@ -23,3 +23,36 @@ var Person = Backbone.Model.extend({
 // person.on('invalid', function(model, error) {
 //     console.log(error);
 // });
+
+var PersonView = Backbone.View.extend({
+	tagName: 'li',
+	// className: 'person',
+	// id: 'some-person',
+	// 
+	template: _.template($("#personTemplate").html()),
+	// template: "#personTemplate",
+
+	initialize: function() {
+		this.render();
+	},
+
+	render: function() {
+		// var template = _.template($(this.template));
+		this.$el.html(this.template(this.model.toJSON()));
+	}
+});
+
+//List of people
+var PeopleCollection = Backbone.Collection.extend({
+	model: Person
+});
+
+var person = new Person;
+var person2 = new Person({ name: 'Larry', age: '25'});
+var personView = new PersonView({ model: person });
+var peopleCollection = new PeopleCollection([
+	{ name: 'Larry', age: '25'},
+	{ name: 'Joe', age: '26'},
+	{ name: 'Megan', age: '27'},
+	{ name: 'Dani', age: '28'}
+]);
