7c8198b012c4d3228f7aa977cf03cb716bdaee6d
Larry Laski <llaski@resolute.com>
2014-04-30 17:45:24 -0400

    updated slide properties

--- slides/app/scripts/views/slide.js
+++ slides/app/scripts/views/slide.js
@@ -5,16 +5,31 @@ define(['backbone'], function(Backbone){
         render : function() {
             if (this.model.get('image')) {
                 this.renderImage();
+            } else if (this.model.get('bullets')) {
+                this.renderBullets();
+            } else {
+                this.renderHeading();
             }
-            this.$el.append(
-                '<h1 class="' + this.model.get('size') + '">' + this.model.get('title')  + '</h1>'
-            );
 
             return this;
         },
 
         renderImage: function() {
             this.$el.addClass('image').append('<img src="' + this.model.get('image') + '">');
+        },
+
+        renderBullets: function() {
+            var $el = this.$el;
+            $el.addClass('bullets');
+
+            if (this.model.get('title')) this.renderHeading();
+            $el.append(['<ul>', '<li>' + this.model.get('bullets').join('</li><li>') + '</li>', '</ul>'].join(''));
+        },
+
+        renderHeading: function() {
+            this.$el.append(
+                '<h1 class="' + this.model.get('size') + '">' + this.model.get('title')  + '</h1>'
+            );
         }
     });
 
