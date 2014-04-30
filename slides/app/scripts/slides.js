732daeb7cd1759839941c20cad3a8447929bea80
Larry Laski <llaski@resolute.com>
2014-04-30 17:45:35 -0400

    slides data

---
+++ slides/app/scripts/slides.js
@@ -0,0 +1,18 @@
+window.slides = [
+    {
+        title : 'My Presentation'
+    },
+    {
+        title : 'Bullets Matter',
+        bullets : ['Point 1', 'Point 2', 'Point 3']
+    },
+    {
+        image : 'http://placekitten.com/600/300'
+    },
+    {
+        title : 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. '
+    },
+    {
+        title : 'My Presentation 3'
+    }
+];
